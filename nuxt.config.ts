f292ed3bcafe61896134cff51e789f0fe47bea26
Georgiy Rzhavin <g.rzhavin@yandex.ru>
2025-02-04 05:17:45 +0000

    Deleted nuxt-directus module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-directus', 'nuxt-quasar-ui', '@pinia/nuxt', '@nuxtjs/i18n', '@nuxtjs/i18n'],
+  modules: ['nuxt-quasar-ui', '@pinia/nuxt', '@nuxtjs/i18n', '@nuxtjs/i18n'],
   experimental: { appManifest: false },
   i18n: {
     strategy: 'no_prefix',
--- stores/AppStore.js
+++ stores/AppStore.js
@@ -2,10 +2,32 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', () => {
     const leftDrawerOpen = ref(true)
-
+    
     function openCloseBar() {
         leftDrawerOpen.value = !leftDrawerOpen.value
     }
 
-    return { leftDrawerOpen, openCloseBar }
+    const accessToken = ref(null)
+    const refreshToken = ref(null)
+    const expires = ref(0)
+
+    const runtimeConfig = useRuntimeConfig()
+
+    async function login(email, password) {
+        const url = runtimeConfig.public.directus.url
+        const { data, status } = await useFetch(`${url}/auth/login`, {
+            method: 'POST',
+            body: {
+                "email": email,
+                "password": password
+            }
+        })
+        if (status === 'success') {
+            accessToken.value = data.value.access_token
+            refreshToken.value = data.value.refresh_token 
+            expires.value = data.value.expires
+        }
+    }
+
+    return { leftDrawerOpen, openCloseBar, login }
 })
